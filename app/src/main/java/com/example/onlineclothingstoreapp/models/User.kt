a3d160665815ce9e6c5724f2661a225a17ed3202
CuocLiz <cuoc2004@gmail.com>
2026-06-03 04:06:32 +0700

    Update User.kt

--- app/src/main/java/com/example/onlineclothingstoreapp/models/User.kt
+++ app/src/main/java/com/example/onlineclothingstoreapp/models/User.kt
@@ -1,7 +1,6 @@
-package com.example.onlineclothingstoreapp.models
-
 data class User(
-    var id: String = "",        // uid từ Firebase
-    var username: String = "",  // tên hiển thị (Tên đăng nhập)
-    var email: String = ""      // email login
+    val id: String = "",
+    val username: String = "",
+    val email: String = "",
+    val createdAt: Long = System.currentTimeMillis()
 )
